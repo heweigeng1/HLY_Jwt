ff93172fab05085bfa6dfda88dd5b8ac6cb1b45c
heweigeng1 <6318275@qq.com>
2018-01-09 18:12:09 +0800

    初始化

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using HLY_Jwt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var token = new JwtUtils().GetToken();
+            new JwtUtils().Parse(token, "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HLY_Jwt/JwtUtils.cs
@@ -0,0 +1,58 @@
+﻿using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HLY_Jwt
+{
+    public class JwtUtils
+    {
+        public string GetToken()
+        {
+            Dictionary<string, object> payload = new Dictionary<string, object>
+            {
+                { "name","tom"},
+                { "age",18}
+            };
+            const string secret = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk";
+
+            IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
+            IJsonSerializer serializer = new JsonNetSerializer();
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+
+            return encoder.Encode(payload, secret);
+        }
+        /// <summary>
+        /// 解析token
+        /// </summary>
+        /// <param name="token">要解析的token</param>
+        /// <param name="secret">密钥</param>
+        public void Parse(string token,string secret)
+        {
+            try
+            {
+                IJsonSerializer serializer = new JsonNetSerializer();
+                IDateTimeProvider provider = new UtcDateTimeProvider();
+                IJwtValidator validator = new JwtValidator(serializer, provider);
+                IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+                IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder);
+
+                var json = decoder.Decode(token, secret, verify: true);
+                Console.WriteLine(json);
+            }
+            catch (TokenExpiredException)
+            {
+                Console.WriteLine("Token has expired");
+            }
+            catch (SignatureVerificationException)
+            {
+                Console.WriteLine("Token has invalid signature");
+            }
+        }
+    }
+}
